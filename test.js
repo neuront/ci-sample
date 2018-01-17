9851a9b3aa9007a0ccddd91b86f0e87b7d071947
Zhe Lin <Lene13@gmail.com>
2018-01-17 14:57:51 +0900

    run mocha programmatically

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var page = require('webpage').create();
 describe('Page load', function() {
     it('loaded', function() {
         page.open('http://localhost:8080/', function (status) {
-	    assert.eq(status, 'CI');
+            assert.eq(status, 'CI');
             phantom.exit();
         });
     });
